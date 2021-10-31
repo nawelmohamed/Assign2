d28396039bc2f668f80a41264565aa672a8da983
nawelmohamed <nawel.mohamed@wethink.info>
2021-10-31 22:56:55 +0100

    modifie color button

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,14 +1,30 @@
 import React from "react";
 import {View, Text, StyleSheet, Button} from 'react-native';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const HomeScreen = ({navigation})=> {
-      return <View>
-          <Button title='Left' onPress={() =>
+      return <View style={Styles.row}>
+          <Button
+          title='Left' 
+          color='red'
+          onPress={() =>
             navigation.navigate('Left')}/>
-          <Button title='Right' onPress={() =>
+          <Button 
+          title='Right'
+          color='blue'
+          onPress={() =>
             navigation.navigate('Right')}/> 
       </View>
 };
-const Styles= StyleSheet.create({});
+const Styles= StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 20,
+    
+        border: 1,
+        borderColor: "gray"
+    }
+});
 
 export default HomeScreen;
