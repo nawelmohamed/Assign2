8a8aa3cb75101e419ef5236318e99df5bfd8d163
nawelmohamed <nawel.mohamed@wethink.info>
2021-11-01 16:16:23 +0100

    styling

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,30 +1,26 @@
 import React from "react";
 import {View, Text, StyleSheet, Button} from 'react-native';
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 const HomeScreen = ({navigation})=> {
-      return <View style={Styles.row}>
-          <Button
+      return <View style={Styles.row} >
+          <Button 
           title='Left' 
-          color='red'
+           
           onPress={() =>
             navigation.navigate('Left')}/>
-          <Button 
-          title='Right'
-          color='blue'
-          onPress={() =>
+          <Button title='Right' onPress={() =>
             navigation.navigate('Right')}/> 
       </View>
 };
 const Styles= StyleSheet.create({
-    row: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        padding: 20,
-    
-        border: 1,
-        borderColor: "gray"
-    }
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+
+    border: 1,
+    borderColor: "gray"
+}
 });
 
 export default HomeScreen;
